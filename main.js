f158717a75bbbbcc86cfdc37c36a28c1cead8baa
AgolaDeveloper <owinoagola00@gmail.com>
2021-02-22 18:06:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById("ht").textContent="Mouse Events";
+document.getElementById("hth").textContent="Mouse Position";
+document.getElementById("hf").textContent="Input Events";
+
+
+//var x= document.getElementsByClassName("grid-area");
+//for(var i=0;i,x.length;i++){
+    //var y= document.createElement("p");
+    //x[i].appendChild(y);
+
+//}
+
+document.getElementById("lis").style.listStyleType= "none";
+document.getElementById("lis2").style.color= "white";
+document.getElementById("lis2").style.backgroundColor= "gray";
+document.getElementById("lis3").style.backgroundColor= "gray";
+document.getElementById("lis3").style.color= "white";
+
+
+
+//document.getElementsByTagName("h2").style.textAlign= "center";
+
+
+
